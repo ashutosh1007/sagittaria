a35d331982ed2eebc96c15370d4d993ac1f75f33
ashutosh1007 <ashutoshtambe1007@gmail.com>
2020-10-06 01:27:48 +0530

    ui updated

--- vendors/js/scripts.js
+++ vendors/js/scripts.js
@@ -1,32 +1,33 @@
-$(document).ready(function(){
-    $("#isotope-filters").on("click", "button", function(){
+
+$(document).ready(function () {
+    $("#isotope-filters").on("click", "button", function () {
         let filterValue = $(this).attr("data-filter");
         $("#isotope").isotope({
             filter: filterValue
         });
-        
         //active button
         $("#isotope-filters").find('.active').removeClass('active');
-        $(this).addClass('active');    
+        $(this).addClass('active');
     });
-});
 
-$(document).ready(function(){
     $("#portfolio-wrapper").magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
+        delegate: 'a'
+        , type: 'image'
+        , gallery: {
             enabled: true
-        },
-        zoom: {
-            enabled: true, 
-            duration: 300,
-            easing: 'ease-in-out', 
-            opener: function(openerElement) {
+        }
+        , zoom: {
+            enabled: true
+            , duration: 300
+            , easing: 'ease-in-out'
+            , opener: function (openerElement) {
                 return openerElement.is('img') ? openerElement : openerElement.find('img');
             }
         }
     });
+    
+     $("#btn-filter").on("click", function() {
+         $("#filters").toggle('slow', 'swing');    
+        
+     });
 });
-
-
